f148958f37ff284a3ea3d542a2f5e5fb8e814cb7
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2022-05-06 18:46:32 -0300

    feat: mail

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
 app.use(express.json());
+
+var transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "30d85ad26bf8fc",
+    pass: "24d17fa9f3f089"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -15,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
       screenshot,
     }
   });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Giovanne Tarcitano <giovannetarcitano@gmail.com>',
+    subject: 'Novo Feedback do usuário',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n'),
+  })
 
   return res.status(201).json({
     data: feedback
